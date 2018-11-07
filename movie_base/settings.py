73582dc081d42db158468f737704bd88cff9d45d
Kaushiksekar <kaushik3993@gmail.com>
2018-11-07 12:04:03 +0530

    Commit for allowed hosts

--- movie_base/settings.py
+++ movie_base/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%hyv^6&jp2(ptvuilp*#r6(p9uyafiz+8o&7(iam34dow$3i-='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
